142ebb001c2a9f4ec0f383b0dd5fbb751b837db2
Jhonattan Curtarelli <Jhonattan.curtarelli@outlook.com>
2025-07-24 20:58:44 -0300

    feat: criando endpoint para healthcheck da api

---
+++ Psycheflow.Api/Controllers/ApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Psycheflow.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("healthcheck")]
+        public IActionResult GetHealthCheck()
+        {
+            object healthInfo = new
+            {
+                status = "Healthy",
+                uptime = GetUptime(),
+                machine = Environment.MachineName,
+                timestamp = DateTime.UtcNow,
+                memoryUsageMB = GetMemoryUsageInMB()
+            };
+
+            return Ok(healthInfo);
+        }
+
+        private string GetUptime()
+        {
+            using Process process = Process.GetCurrentProcess();
+            TimeSpan uptime = DateTime.Now - process.StartTime;
+            return uptime.ToString(@"dd\.hh\:mm\:ss");
+        }
+
+        private double GetMemoryUsageInMB()
+        {
+            using var process = Process.GetCurrentProcess();
+            return Math.Round(process.WorkingSet64 / (1024.0 * 1024.0), 2);
+        }
+    }
+}
