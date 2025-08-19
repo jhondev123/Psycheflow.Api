661eda4b02fe68ae7e043eae9244ed8f2750e115
jhondev123 <jhonattan.curtarelli@outlook.com>
2025-08-18 21:34:12 -0300

    feat: criando novas migrations

--- Psycheflow.Api/Seeders/CompanySeeder.cs
+++ Psycheflow.Api/Seeders/CompanySeeder.cs
@@ -1,16 +1,36 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Psycheflow.Api.Contexts;
+using Psycheflow.Api.Entities;
+
 namespace Psycheflow.Api.Seeders
 {
     public class CompanySeeder : ISeeder
     {
+        private AppDbContext Context { get; set; }
+
         public bool onlyHomolog => true;
-        public Task Up()
+
+        public CompanySeeder(AppDbContext context)
         {
-            throw new NotImplementedException();
+            Context = context;
         }
-        public Task Down()
+        public async Task Up()
         {
-            throw new NotImplementedException();
+            Company company = new Company
+            {
+                Id = new Guid("266E089A-F2C2-40BA-539D-08DDCEF1516A"),
+                Name = "teste",
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            };
+            await Context.AddAsync(company);
+            Context.SaveChanges();
+        }
+        public async Task Down()
+        {
+            await Context.Companies.ExecuteDeleteAsync();
         }
 
     }
---
+++ Psycheflow.Api/Seeders/UserSeeder.cs
@@ -0,0 +1,39 @@
+﻿
+using Psycheflow.Api.Contexts;
+using Psycheflow.Api.Entities;
+
+namespace Psycheflow.Api.Seeders
+{
+    public class UserSeeder : ISeeder
+    {
+        private AppDbContext Context { get; set; }
+
+        public bool onlyHomolog => true;
+
+        public UserSeeder(AppDbContext context)
+        {
+            Context = context;
+        }
+        public Task Up()
+        {
+            User user = new User
+            {
+                Id = "149e039e-e59e-46db-8b5c-e8294ae4a69f",
+                UserName = "Test",
+                Email = "test@gmail.com",
+                NormalizedEmail = "test@gmail.com",
+                CompanyId = new Guid("149e039e-e59e-46db-8b5c-e8294ae4a69f"),
+                BirthDate = DateTime.Now,
+                PasswordHash = "AQAAAAIAAYagAAAAEJzKBFfdRz3qSX7whKQYjlaNBlskzX6Tnena89xHax78SZWeYc+/pO8rR8wsl19bpg==",
+                
+                
+
+
+            };
+        }
+        public Task Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
