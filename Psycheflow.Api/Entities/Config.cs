f4350849fc066d3b1b13545dbf2039c469cb66ef
Jhonattan Curtarelli <Jhonattan.curtarelli@outlook.com>
2025-08-30 15:00:43 -0300

    feat: criando entidade config

---
+++ Psycheflow.Api/Entities/Config.cs
@@ -0,0 +1,14 @@
+﻿namespace Psycheflow.Api.Entities
+{
+    public class Config : BaseEntity
+    {
+        public Guid CompanyId { get; set; }
+        public Company? Company { get; set; }
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+        public string Key { get; set; }
+        public string? Value { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
